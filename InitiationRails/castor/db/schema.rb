c04cbe311de16a0ea66e5d5e42725074d40debff
Simon Fuet <simon.fuet@ephesia-consult.com>
2019-07-29 13:01:14 +0200

    Foreign keys

--- InitiationRails/castor/db/schema.rb
+++ InitiationRails/castor/db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_164548) do
+ActiveRecord::Schema.define(version: 2019_07_29_104920) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_books_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
     t.string "title"
   end
 
